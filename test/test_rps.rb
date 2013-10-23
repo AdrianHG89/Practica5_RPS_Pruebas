6dd784c044879803ed592ae21ba33b3642d222ac
Adrian <adrihg89@gmail.com>
2013-10-23 13:37:53 +0100

    Assertions para la zona destaca y el cuerpo principal

--- test/test_rps.rb
+++ test/test_rps.rb
@@ -16,11 +16,19 @@ class Test_RPS < Test::Unit::TestCase
 		assert last_response.ok?
 	end
 
-	def test_body
+	def test_hero_unit
 		get "/"
-		assert last_response.body.include?("Maquina"), "El body debe contener Maquina."
-		assert last_response.body.include?("Vs"), "El body debe contener Vs."
-		assert last_response.body.include?("Jugador"), "El body debe contener Jugador."
+		assert last_response.body.include?("<h1>Juega a Piedra, Papel o Tijera</h1>"), "La zona destacada debe tener este mensaje: Juega a Piedra, Papel o Tijera'."
+		assert last_response.body.include?("<a class='btn btn-large btn-primary active' href='?jugada=Piedra'>Piedra</a>"), "La zona destacada debe tener enlace/boton Piedra."
+		assert last_response.body.include?("<a class='btn btn-large btn-primary active' href='?jugada=Papel'>Papel</a>"), "La zona destacada debe tener enlace/boton Papel."
+		assert last_response.body.include?("<a class='btn btn-large btn-primary active' href='?jugada=Tijeras'>Tijeras</a>"), "La zona destacada debe tener enlace/boton Tijera."
+	end
+
+	def test_row
+		get "/"
+		assert last_response.body.include?("<h2>Maquina</h2>"), "El body debe contener Maquina."
+		assert last_response.body.include?("<h2>Vs.</h2>"), "El body debe contener Vs."
+		assert last_response.body.include?("<h2>Jugador</h2>"), "El body debe contener Jugador."
 	end
 
 end
